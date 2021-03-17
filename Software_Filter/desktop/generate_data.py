972212a002bcc8f28ac9b2cc7e544f19c5d454f8
LQ1234 <larryqiu424@yahoo.com>
2021-03-17 18:39:12 -0400

    improve data generator

--- Software_Filter/desktop/generate_data.py
+++ Software_Filter/desktop/generate_data.py
@@ -1,22 +1,79 @@
 from pydub import AudioSegment
-from glob import glob
+from pathlib import Path
 from pydub.utils import get_array_type
 import array
+import argparse
+import math
 
-sample_rate = 16000
+parser = argparse.ArgumentParser(
+    description="Convert sound file to list of samples")
+parser.add_argument("-s", "--sample_rate", type=int, default=16000,
+                    help="sample rate")
+parser.add_argument("-l", "--max_length", type=float,
+                    help="max length")
+parser.add_argument("-n", "--noise", type=float, default=0,
+                    help="noise (0 = no noise, 1 = equal sound and noise, >1 = more noise)")
+parser.add_argument("-f", "--noise_file", type=str, default="data/other/backgroundnoise.mp3",
+                    help="noise file")
+parser.add_argument("-o", "--output_file", type=str, default="data.txt",
+                    help="output file")
+parser.add_argument("-os", "--output_sound_file", type=str,
+                    help="output sound file")
+parser.add_argument("input_files", default=[
+                    "data/*"], nargs="*", help="input files")
 
-with open("data.txt", "w") as f:
+args = parser.parse_args()
+print("sample_rate =", args.sample_rate, "max_length =", args.max_length, "noise =",
+      args.noise, "noise_file =", args.noise_file, "output_file =", args.output_file, "output_sound_file =",args.output_sound_file)
 
-    for file in glob("data/A1-0001_Bare Whistle Sounds00000228.mp4"):
+
+sample_rate = args.sample_rate // 2  # for some reason it needs to be halved
+
+if(args.noise > 0):
+    db_diff = math.log(args.noise)
+    noise_path = Path(args.noise_file)
+    noise = AudioSegment.from_file(noise_path)
+else:
+    noise_path = None
+    noise = None
+files = set()
+for globs in args.input_files:
+    for file in Path.cwd().glob(globs):
+        if(noise is not None and file.samefile(noise_path)):
+            continue
+        files.add(file)
+
+alloc_left = args.max_length or 1000000000
+
+if(args.output_sound_file):
+    output_sound = AudioSegment.silent(duration=0)
+else:
+    output_sound = None
+with open(args.output_file, "w") as f:
+    for file in files:
         # https://stackoverflow.com/a/32383120/5771000
-        sound = AudioSegment.from_file(file)
-        sound_resample = sound.set_frame_rate(sample_rate)
-        print(file,"length:",sound_resample.duration_seconds,"seconds")
+        sound = AudioSegment.from_file(file)[:int(alloc_left * 1000)]
+        alloc_left -= sound.duration_seconds
+        if noise is not None:
+            sound_with_noise = sound.overlay(noise, loop=True)
+        else:
+            sound_with_noise = sound
+        sound_resample = sound_with_noise.set_frame_rate(sample_rate)
+
+        print(file, "length:", sound_resample.duration_seconds, "seconds")
 
         bit_depth = sound_resample.sample_width * 8
         array_type = get_array_type(bit_depth)
 
-        arr= array.array(array_type, sound_resample._data)
+        arr = array.array(array_type, sound_resample._data)
+
         for x in arr:
             f.write(str(x))
             f.write("\n")
+
+        if output_sound is not None:
+            output_sound += sound_resample
+        if alloc_left <= 0.01:
+            break
+if output_sound is not None:
+    output_sound.export(args.output_sound_file, format="wav")
--- Software_Filter/desktop/main.cpp
+++ Software_Filter/desktop/main.cpp
@@ -1,3 +1,4 @@
+// python generate_data.py -n 1 -l 20 -os "sound.wav" data/A1*
 // g++ main.cpp arduinoFFT/arduinoFFT.cpp -o main -Wno-#warnings && ./main && python display_spectrogram.py
 
 #import "arduinoFFT/arduinoFFT.h"
@@ -26,6 +27,14 @@ const uint16_t sampling_frequency = 16000;
 const uint16_t num_samples = 512;
 const uint16_t iters = 100;
 
+const float freq_cutoff_low = 2000;
+const float freq_cutoff_high = 4000;
+const float freq_neighbor = 500;
+
+const int low_index = freq_cutoff_low * num_samples / sampling_frequency;
+const int high_index = freq_cutoff_high * num_samples / sampling_frequency;
+const int neighbor_index = freq_neighbor * num_samples / sampling_frequency;
+
 int sample_index=0;
 
 double real_buf[num_samples];
@@ -38,14 +47,21 @@ unsigned long start;
 
 
 void process_fft(){
+    // sampling_frequency/num_samples*i = freq
+
     for(int i=0; i<num_samples/2; i++){
         outfile << real_buf[i] << " ";
     }
-    if(real_buf[95]>1000){
-        outfile << "y";
-    }else{
-        outfile << "n";
+
+
+    int max = low_index;
+    for(int i=low_index+1; i<=high_index; i++){
+        if(real_buf[i]>real_buf[max]){
+            max = i;
+        }
     }
+
+
 
     outfile <<"\n";
 }
@@ -68,9 +84,11 @@ void process_sample(int16_t sample){
 }
 
 int main(int argc, char const *argv[]) {
+    std::cout<<"start\n";
     outfile.open("spectrogram.txt");
 
     std::vector<int16_t> data = read_data("data.txt");
+    std::cout<<"low "<<low_index<<" high "<<high_index<<" neighbor "<<neighbor_index<<"\n";
 
     for(int i=0;i<data.size();i++){
         process_sample(data[i]);
